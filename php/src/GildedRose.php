4c6385f22748967c0a44d977c87b1ac32de0769c
rick91236rick <rick91236rick@gmail.com>
2022-04-01 13:47:21 +0800

    將相同判斷式邏輯合併

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -34,6 +34,12 @@ final class GildedRose
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
                 }
+
+                if ($item->sell_in < 0) {
+                    if ($item->quality < 50) {
+                        $item->quality = $item->quality + 1;
+                    }
+                }
             } elseif ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
@@ -48,25 +54,16 @@ final class GildedRose
                         }
                     }
                 }
+                if ($item->sell_in < 0) {
+                    $item->quality = 0;
+                }
             } else {
                 if ($item->quality > 0) {
                     $item->quality = $item->quality - 1;
                 }
-            }
-
-
-            if ($item->sell_in < 0) {
-                if ($item->name == 'Aged Brie') {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                } else {
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        $item->quality = $item->quality - $item->quality;
-                    } else {
-                        if ($item->quality > 0) {
-                            $item->quality = $item->quality - 1;
-                        }
+                if ($item->sell_in < 0) {
+                    if ($item->quality > 0) {
+                        $item->quality = $item->quality - 1;
                     }
                 }
             }
